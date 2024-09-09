9490eef5dc937f117e4c816a3be2cf926b85e906
Rodrigo Gomes <gomesrodrigo0481@gmail.com>
2024-09-09 19:43:38 -0300

    scheduler-arquivar-pedido movido para projeto batch-jobs

--- terraform/scheduler-arquivar-pedido/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lambdas" {
-  value = [{
-    arn          = aws_lambda_function.arquivar_pedido.arn
-    name         = aws_lambda_function.arquivar_pedido.function_name
-    description  = aws_lambda_function.arquivar_pedido.description
-    version      = aws_lambda_function.arquivar_pedido.version
-    last_modified = aws_lambda_function.arquivar_pedido.last_modified
-  }]
-}
